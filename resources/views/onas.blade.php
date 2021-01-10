88038e6b50c7315cb7f19c2c0355399cbb14ae15
Piotr Zieliński <zzieluuuu@gmail.com>
2021-01-10 22:06:26 +0100

    test

--- resources/views/onas.blade.php
+++ resources/views/onas.blade.php
@@ -11,21 +11,16 @@
                   <h2 class="site-section-heading text-center">O Nas</h2>
                 </div>
               </div>
-
                <div class="row mb-5">
                 <div class="col-md-7">
                   <img src="/storage/images/img_2.jpeg" alt="Images" class="img-fluid">
                 </div>
                 <div class="col-md-4 ml-auto">
                   <h3>Nasza Misja</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus vel tenetur explicabo necessitatibus, ad soluta consectetur illo qui voluptatem. Quis soluta maiores eum. Iste modi voluptatum in repudiandae fugiat suscipit dolorum harum, porro voluptate quis? Alias repudiandae dicta doloremque voluptates soluta repellendus, unde laborum quo nam, ullam facere voluptatem similique.</p>
+                  <p>Lorem ipsum dolor  sit amet, consectetur adipisicing elit. Possimus vel tenetur explicabo necessitatibus, ad soluta consectetur illo qui voluptatem. Quis soluta maiores eum. Iste modi voluptatum in repudiandae fugiat suscipit dolorum harum, porro voluptate quis? Alias repudiandae dicta doloremque voluptates soluta repellendus, unde laborum quo nam, ullam facere voluptatem similique.</p>
                 </div>
               </div>
-
-
-
             </div>
-
           </div>
         </div>
       </div>
